9ea35b918a72d743b2878110c10a72d9f786899a
liptonzuma <solomonboateng231@gmail.com>
2024-01-31 00:42:58 +0000

    pushing chnages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import app from './firebaseConfig';
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function registerAndLogin() {
+    setLoading(true);
+    try {
+      const auth = getAuth(app);
+      await createUserWithEmailAndPassword(auth, email, password);
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      setLoading(false);
+      Alert.alert('Success', response.user.uid);
+      return;
+    } catch (error) {
+      setLoading(false);
+      Alert.alert('Ooops', 'something went wrong');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={[styles.input, { marginTop: 15 }]}
+        placeholder="Password"
+        onChangeText={setPassword}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={registerAndLogin}>
+        {loading ? (
+          <ActivityIndicator
+            size={'small'}
+            color={'white'}
+            animating={loading}
+          />
+        ) : (
+          <Text style={{ color: 'white' }}>Login</Text>
+        )}
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    width: '90%',
+    height: 45,
+    backgroundColor: 'teal',
+    borderRadius: 6,
+    marginTop: 25,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '90%',
+    height: 45,
+    borderRadius: 6,
+    paddingHorizontal: 10,
+    backgroundColor: '#f5f5f5',
+    alignSelf: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB2QhgM5EL3XtRaUyz27qGjD6exSxCI7NQ',
+  authDomain: 'fir-starter-c04ed.firebaseapp.com',
+  projectId: 'fir-starter-c04ed',
+  storageBucket: 'fir-starter-c04ed.appspot.com',
+  messagingSenderId: '435896240723',
+  appId: '1:435896240723:web:9f7ff1388506734d58d4a6',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
